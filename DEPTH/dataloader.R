3249a6d15fc1b8eba0cfc80a3e0a58152301b34c
XinY-Z <animintegrius84260@gmail.com>
2022-10-04 23:04:58 -0600

    Added missing data analysis. Added alliance as covariate. Tested all cross-level interactions. Tested group effect of coefficients using Bayesian.

--- DEPTH/dataloader.R
+++ DEPTH/dataloader.R
@@ -3,3 +3,6 @@ library(data.table)
 set.seed(123)
 #load('image_062422.RData')
 load(r'(C:\Users\XinZ\Box\DATA\Xin PDRP\image_062422.RData)')
+rm(svr_intv1)
+
+dt[, ave_empathy := mean(empathy), client_epi]
